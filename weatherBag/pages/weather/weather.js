3ae1a044f0c46650dfc9a5ef86a065807f7fe55d
姚晓辉 <283251605@qq.com>
2019-04-23 18:16:45 +0800

    天气bug

--- weatherBag/pages/weather/weather.js
+++ weatherBag/pages/weather/weather.js
@@ -24,49 +24,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // 获取用户信息
-    var heihei=this
-    wx.getUserInfo({
-      success(res) {
-        // console.log(res)
-        const userInfo = res.userInfo
-        const nickName = userInfo.nickName
-        const avatarUrl = userInfo.avatarUrl
-        const gender = userInfo.gender // 性别 0：未知、1：男、2：女
-        const province = userInfo.province
-        const city = userInfo.city
-        const country = userInfo.country
-        heihei.setData({
-          xingbie: userInfo.gender
-        })
-      }
-    })
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
     //天氣
     var that = this;
     // 新建百度地图对象 
@@ -91,6 +48,49 @@ Page({
       fail: fail,
       success: success
     }); 
+    // // 获取用户信息
+    // var heihei = this;
+    // wx.getUserInfo({
+    //   success(res) {
+    //     // console.log(res)
+    //     const userInfo = res.userInfo
+    //     const nickName = userInfo.nickName
+    //     const avatarUrl = userInfo.avatarUrl
+    //     const gender = userInfo.gender // 性别 0：未知、1：男、2：女
+    //     const province = userInfo.province
+    //     const city = userInfo.city
+    //     const country = userInfo.country
+    //     heihei.setData({
+    //       xingbie: userInfo.gender
+    //     })
+    //   }
+    // })
+    // if (app.globalData.userInfo) {
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true
+    //   })
+    // } else if (this.data.canIUse) {
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //   // 所以此处加入 callback 以防止这种情况
+    //   app.userInfoReadyCallback = res => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //       hasUserInfo: true
+    //     })
+    //   }
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true
+    //       })
+    //     }
+    //   })
+    // }
   },
   // 关闭信息弹框
   goClose: function () {
@@ -107,11 +107,13 @@ Page({
     })
   },
   getUserInfo: function (e) {
-    // console.log(e)
+    // console.log(e.detail.rawData);
+    var gender = JSON.parse(e.detail.rawData);
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      hasUserInfo: true,
+      xingbie: gender.gender
     })
   },
   /**
